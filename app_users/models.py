8bbdea360f079b89dc6b67076cad6078ea90120a
ramavatar26 <kaswanramavatar@gmail.com>
2021-09-09 22:54:07 +0530

    Initial Commit

---
+++ app_users/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import User
+import os
+from django.urls import reverse
+# Create your models here.
+
+def path_and_rename(instance, filename):
+    upload_to = 'Images/'
+    ext = filename.split('.')[-1]
+    if instance.user.username:
+        filename = 'User_Profile_Picture/{}.{}'.format(instance.user.username, ext)
+    return os.path.join(upload_to, filename)
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    bio = models.CharField(max_length=300, blank=True)
+
+    profile_Pic = models.ImageField(upload_to=path_and_rename,verbose_name="Profile Picture", blank=True);
+
+    teacher = 'teacher'
+    student = 'student'
+    parent = 'parent'
+    user_types = [
+        (teacher, 'teacher'),
+        (student, 'student'),
+        (parent, 'parent'),
+    ]
+    user_type = models.CharField(max_length=10, choices=user_types, default=student)
+
+    def __str__(self):
+        return self.user.username
+
+class Contact(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+    feedback = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('index')
---
+++ app_users/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from app_users.forms import UserForm, UserProfileInfoForm
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView, CreateView
+from curriculum.models import Standard
+from .models import UserProfileInfo, Contact
+# Create your views here.
+
+# def index(request):
+#     return render(request,'base.html')
+
+
+def register(request):
+
+    registered = False
+
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return render(request, 'app_users/registration.html',
+                            {'registered':registered,
+                             'user_form':user_form,
+                             'profile_form':profile_form})
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("ACCOUNT IS DEACTIVATED")
+        else:
+            return HttpResponse("please use correct id and password")
+
+    else:
+        return render(request, 'app_users/login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+class HomeView(TemplateView):
+    template_name = 'app_users/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        standards = Standard.objects.all()
+        teachers = UserProfileInfo.objects.filter(user_type='teacher')
+        context['standards'] = standards
+        context['teachers'] = teachers
+        return context
+
+class ContactView(CreateView):
+    model = Contact
+    fields = '__all__'
+    template_name = 'app_users/contact.html'
